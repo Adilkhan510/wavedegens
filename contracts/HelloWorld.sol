c4907246c4bb10441625d879e38d40e56fb63807
Adil Khan <54783965+Adilkhan510@users.noreply.github.com>
2022-02-19 20:39:40 -0800

    init commit

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: Unlicense
+
+//Set compiler for the contract
 pragma solidity ^0.8.0;
+
 
 contract HelloWorld {
     function hello() public pure returns (string memory) {
+
+        //pure = doesn't mutate state
+        // returns a string 
         return "Hello, World";
     }
 }
---
+++ tests/test.ts
@@ -0,0 +1,4 @@
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import { ethers } from "hardhat";
+import { expect } from "chai";
